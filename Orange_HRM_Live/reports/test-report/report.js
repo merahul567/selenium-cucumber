57697a9cf87307fdb7b438128e3fc768e37726a7
kumar <merahul567@gmail.com>
2021-01-18 14:03:00 +0530

    Test in CG laptop

--- Orange_HRM_Live/reports/test-report/report.js
+++ Orange_HRM_Live/reports/test-report/report.js
@@ -39,7 +39,7 @@ formatter.match({
   "location": "StepDefinition.navigate()"
 });
 formatter.result({
-  "duration": 20908778100,
+  "duration": 11460822999,
   "status": "passed"
 });
 formatter.match({
@@ -56,14 +56,14 @@ formatter.match({
   "location": "StepDefinition.login(String,String)"
 });
 formatter.result({
-  "duration": 11521973900,
+  "duration": 10261114100,
   "status": "passed"
 });
 formatter.match({
   "location": "StepDefinition.verifySuccessful()"
 });
 formatter.result({
-  "duration": 78056900,
+  "duration": 82994001,
   "status": "passed"
 });
 formatter.scenarioOutline({
@@ -177,14 +177,14 @@ formatter.match({
   "location": "StepDefinition.User_is_in_to_My_Info_Page()"
 });
 formatter.result({
-  "duration": 5859535000,
+  "duration": 3466732200,
   "status": "passed"
 });
 formatter.match({
   "location": "StepDefinition.User_clicked_on_Edit_button()"
 });
 formatter.result({
-  "duration": 1115317600,
+  "duration": 1159512501,
   "status": "passed"
 });
 formatter.match({
@@ -197,14 +197,14 @@ formatter.match({
   "location": "StepDefinition.User_changes_the_first_name(String)"
 });
 formatter.result({
-  "duration": 1561854900,
+  "duration": 1278707400,
   "status": "passed"
 });
 formatter.match({
   "location": "StepDefinition.User_clicked_on_save_button()"
 });
 formatter.result({
-  "duration": 3954007100,
+  "duration": 3113265299,
   "status": "passed"
 });
 formatter.match({
@@ -217,7 +217,7 @@ formatter.match({
   "location": "StepDefinition.updated_first_name_should_be_diplayed(String)"
 });
 formatter.result({
-  "duration": 1169028600,
+  "duration": 1178678900,
   "status": "passed"
 });
 });
